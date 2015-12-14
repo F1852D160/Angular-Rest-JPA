d759019253bce3ebff68074cfb38aa245231e579
Mitsu Gami <migamipe@outlook.com>
2015-12-14 08:16:14 -0500

    Update ApplicationConfig.java

--- src/main/java/cl/clarochile/intranet/bootstrap/ApplicationConfig.java
+++ src/main/java/cl/clarochile/intranet/bootstrap/ApplicationConfig.java
@@ -1,12 +1,8 @@
 package cl.clarochile.intranet.bootstrap;
 
-//import java.util.HashSet;
-//import java.util.Set;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-//import cl.clarochile.intranet.rests.UserRest;
 
 /**
  * 
@@ -18,10 +14,5 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("api")
 public class ApplicationConfig extends Application {
 
-	/*@Override
-	public Set<Class<?>> getClasses() {
-		final Set<Class<?>> classes = new HashSet<>();
-		classes.add(UserRest.class);
-		return classes;
-	}*/
+
 }
